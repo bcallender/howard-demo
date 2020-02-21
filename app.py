b71b6bacbfe65b09cf9efbc07d0cd6bc97df1811
Brandon Callender <brandoncallender@salesforce.com>
2020-02-21 17:05:07 -0500

    howard name

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-
+import settings as s
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello {}!'.format(s.NAME)
 
 
 if __name__ == '__main__':
